eda8431dce163b92b3f28de68ccb47553cb2c348
kuresti <55415865+kuresti@users.noreply.github.com>
2024-12-02 12:21:02 -0700

    Create index.js
    -Added index.js to routes folder

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+/* ******************************
+ * Required Resources
+ * ******************************/
+const express = require('express');
+const router = require('express').Router();
+
+/* ******************************
+ * Routes
+ * ******************************/
+
+
+/* ******************************
+ * Use route for Star Wars Vehicles
+ * ******************************/
+router.use('/vehicles', require('./vehicles'));
