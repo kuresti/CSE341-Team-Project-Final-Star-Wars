7142112fa259e6b9a818e9f76166af7a5fd19d36
DC-CC-21 <handstands1000@gmail.com>
2024-12-04 13:06:04 -0500

    add swagger documentation.

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ const mongodb = require('./config/db');
 env.config();
 
 /* *******************************
+ * Swagger
+ * *******************************/
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+/* *******************************
  * Routes
  * *******************************/
 app
