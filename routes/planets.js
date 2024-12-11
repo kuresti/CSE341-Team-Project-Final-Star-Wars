50d2c2d469a75e9bac73414d2b4d6d165292f733
ijromerob <ijromerob@gmail.com>
2024-12-10 19:44:50 -0700

    added validation and tests for routes

--- routes/planets.js
+++ routes/planets.js
@@ -4,17 +4,43 @@
 const express = require('express');
 const router = require('express').Router();
 const planetsController = require('../controllers/planets');
+const {
+  planetRules,
+  validatePlanet,
+  validateObjectId
+} = require('../validation/planetValidation');
 
+/********************************
+ * GET Routes
+ ********************************/
 router.get('/', planetsController.getAllPlanets);
 
 router.get('/location', planetsController.getPlanetByLocation);
 
-router.get('/:id', planetsController.getPlanet);
+router.get('/:id', validateObjectId, planetsController.getPlanet);
 
-router.post('/', planetsController.addPlanet);
+/*******************************
+ * POST Route
+ *******************************/
 
-router.put('/:id', planetsController.editPlanet);
+router.post('/', planetRules(), validatePlanet, planetsController.addPlanet);
 
-router.delete('/:id', planetsController.deletePlanet);
+/******************************
+ * PUT Route
+ ******************************/
+
+router.put(
+  '/:id',
+  validateObjectId,
+  planetRules(),
+  validatePlanet,
+  planetsController.editPlanet
+);
+
+/*****************************
+ * DELETE Route
+ *****************************/
+
+router.delete('/:id', validateObjectId, planetsController.deletePlanet);
 
 module.exports = router;
