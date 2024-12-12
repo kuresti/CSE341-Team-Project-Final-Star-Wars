ded1518ba48ff81d13c75a3a90b24692403f6455
kuresti <55415865+kuresti@users.noreply.github.com>
2024-12-12 13:16:01 -0700

    testing
    Working on server.js testing

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,17 +1,25 @@
-// /* ************************************
-//  * Required Resources
-//  * ************************************/
-// const httpMocks = required('node-mocks-http');
-// const request = require('supertest');
-// const express = require('express');
-// const app = require('../server');
-// const passport = passport('passport');
-// const mongodb = require('../config/db');
+/* ************************************
+ * Required Resources
+ * ************************************/
+const httpMocks = require('node-mocks-http');
+const request = require('supertest');
+const express = require('express');
+const app = require('../server');
+const passport = require('passport');
+const mongodb = require('../config/db');
 
-// /* ************************************
-//  * Mock Database
-//  * ************************************/
-// jest.mock('../config/db.js');
+/* ************************************
+ * Mock Database
+ * ************************************/
+jest.mock('../config/db.js');
 
-// /* ************************************
-//  * 
+/* ************************************
+ * Test Scripts
+ * ************************************/
+describe('Server Tests', () => {
+    it('should return "Logged Out" when no session is running', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Logged Out');
+    });
+});
