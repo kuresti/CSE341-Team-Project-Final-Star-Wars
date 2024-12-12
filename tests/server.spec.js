f73a546eeafe26f81ef7692263e8b1f09737be28
kuresti <55415865+kuresti@users.noreply.github.com>
2024-12-12 12:24:43 -0700

    Create server.spec.js
    _Commented out code so I can test another branch

---
+++ tests/server.spec.js
@@ -0,0 +1,17 @@
+// /* ************************************
+//  * Required Resources
+//  * ************************************/
+// const httpMocks = required('node-mocks-http');
+// const request = require('supertest');
+// const express = require('express');
+// const app = require('../server');
+// const passport = passport('passport');
+// const mongodb = require('../config/db');
+
+// /* ************************************
+//  * Mock Database
+//  * ************************************/
+// jest.mock('../config/db.js');
+
+// /* ************************************
+//  * 
